d92cad369bcd20a37f5777696d2a07c02eb552b1
Victor Eyer <eyervictor@gmail.com>
2021-03-19 11:30:31 -0300

    feat: starts next with server

---
+++ sosvacinacampos/server.js
@@ -0,0 +1,34 @@
+const { createServer } = require('http')
+const whatsapp = require('./configs/whatsapp')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = new URL(req.url, 'http://w.w')
+    const { pathname, query } = parsedUrl
+
+    switch (pathname) {
+      case '/a':
+        app.render(req, res, '/index', query)
+        break;
+      case '/b':
+        app.render(req, res, '/b', query)
+        break;
+      case '/api/whatsapp':
+        req.whatsapp = whatsapp
+        handle(req, res, parsedUrl)
+        break;
+      default:
+        handle(req, res, parsedUrl)
+        break;
+    }
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})

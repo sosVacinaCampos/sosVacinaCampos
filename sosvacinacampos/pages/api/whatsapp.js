8a6a919a1ccd85cb2fd644e974e27e5aa913122c
Victor Eyer <eyervictor@gmail.com>
2021-03-19 11:29:59 -0300

    feat: whatsapp endpoint & configuration

---
+++ sosvacinacampos/pages/api/whatsapp.js
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async (req, res) => {
+  const { body } = req
+
+  if (!!!Object.keys(body).length || !body || !body.number || !body.message) {
+    return res.status(401).json({ error: 'Bad request' })
+  }
+
+  const whatsapp = await req.wpp
+
+  const number = `${body.number.replace('+', '')}@c.us`
+
+  whatsapp.sendText(number, 'mensagem legal')
+
+  return res.status(200).json({ success: 'Sending message' })
+}

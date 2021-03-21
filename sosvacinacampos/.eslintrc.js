49fc6872e8147beb14a8a0980cbd2ed0e792d53f
Victor Eyer <eyervictor@gmail.com>
2021-03-21 19:55:25 -0300

    chore: repo lint & config

---
+++ sosvacinacampos/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'no-underscore-dangle': 0,
+  },
+};
